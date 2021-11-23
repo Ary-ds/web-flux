b11aee4ce91a422685d41e882b69f66dd70937ae
iven ari <ivenari.dacruz@versia.com>
2021-11-23 10:34:07 +0100

    configuraciones basicas

---
+++ src/test/java/com/ary/webFlux/app/SpringBootWebFluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ary.webFlux.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebFluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
